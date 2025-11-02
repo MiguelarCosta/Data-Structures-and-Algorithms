ab70cc33a6978279dbb14e2fefc39744621fcc7d
Miguel Costa <a21250735@isec.pt>
2025-11-02 22:25:09 +0000

    Exercises 1 to 6 from BinarySearch

--- BinarySearch/src/pt/miguel/binarysearch/BinarySearch.java
+++ BinarySearch/src/pt/miguel/binarysearch/BinarySearch.java
@@ -32,6 +32,7 @@ public class BinarySearch {
         return -1;
     }
 
+
     /**
      * Performs a recursive binary search on a sorted array.
      *
@@ -54,5 +55,113 @@ public class BinarySearch {
         else
             return recursiveSearch(Arrays.copyOfRange(arr,0,mid), target);
 
+    }
+
+    // Ficha 2 - Ex 1
+    /**
+     * Checks if a target value exists in the array using recursive binary search.
+     *
+     * @param arr    The sorted array to search.
+     * @param target The value to search for.
+     * @return True if the target is found, false otherwise.
+     */
+    public static boolean ficha2ex1(int[] arr, int target){
+        if(arr == null || arr.length == 0)
+            return false;
+        int mid = arr.length/2;
+        if(arr[mid] == target)
+            return true;
+        if(arr[mid]<target)
+            return ficha2ex1(Arrays.copyOfRange(arr,mid+1,arr.length), target);
+        else
+            return ficha2ex1(Arrays.copyOfRange(arr,0,mid), target);
+    }
+    // Ficha 2 - Ex 2
+    /**
+     * Checks if a target value exists in the array using iterative binary search.
+     *
+     * @param arr    The sorted array to search.
+     * @param target The value to search for.
+     * @return True if the target is found, false otherwise.
+     */
+    public static boolean ficha2ex2(int[] arr, int target){
+        if(arr == null || arr.length == 0)
+            return false;
+
+        int start = 0;
+        int end = arr.length-1;
+        int mid = end/2;
+
+        do{
+            if(arr[mid] == target)
+                return true;
+            if(arr[mid] > target)
+                end = mid-1;
+            if(arr[mid] < target)
+                start = mid+1;
+
+            mid = (start+end)/2;
+        }while (start<=end);
+
+        return false;
+    }
+
+    // Ficha 2 - Ex 3
+    /**
+     * Finds the index of the first occurrence of the target value in the array.
+     *
+     * @param arr    The sorted array to search.
+     * @param target The value to search for.
+     * @return The index of the first occurrence of the target, or -1 if not found.
+     */
+    public static int ficha2ex3(int[] arr, int target){
+        if(arr == null || arr.length == 0)
+            return -1;
+
+        int start = 0;
+        int end = arr.length-1;
+        int mid = end/2;
+
+        do{
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;
+
+            mid = (start+end)/2;
+        }while (start<=end);
+        return -1;
+    }
+
+    // Ficha 2 - Ex 4
+    /**
+     * Finds the index of the first occurrence of the target value in the array.
+     *
+     * @param arr    The sorted array to search.
+     * @param target The value to search for.
+     * @return The index of the first occurrence of the target, or -X if not found, X being the position where it should be inserted.
+     */
+    public static int ficha2ex4(int[] arr, int target) {
+        if (arr == null || arr.length == 0)
+            return -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = end / 2;
+
+        do {
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+
+            mid = (start + end) / 2;
+        } while (start <= end);
+        return -start-1;
     }
 }
--- BinarySearch/src/pt/miguel/binarysearch/Main.java
+++ BinarySearch/src/pt/miguel/binarysearch/Main.java
@@ -3,9 +3,77 @@ package pt.miguel.binarysearch;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void Exercicio1(){
+        int [] arr = {1,3,5,6};
+        int target = 5;
+        boolean res = BinarySearch.ficha2ex1(arr, target);
+        System.out.println("Exercio 1: " + (res ? "Target found!" : "Target not found!"));
+    }
 
-        BinarySearch.iterativeSearch(new int[]{1, 2, 3, 4, 5}, 3);
+    public static void Exercicio2(){
+        int [] arr = {1,3,5,6};
+        int target = 5;
+        boolean res = BinarySearch.ficha2ex2(arr, target);
+        System.out.println("Exercio 2: " + (res ? "Target found!" : "Target not found!"));
+    }
+
+    public static void Exercicio3(){
+        int [] arr = {1,3,5,6};
+        int target = 5;
+        int res = BinarySearch.ficha2ex3(arr, target);
+        System.out.println("Exercio 3: " + (res >= 0 ? "Target found at index: " + res : "Target not found!"));
+    }
+    public static void Exercicio4(){
+        int [] arr = {3,7,12,15};
+        int target = 3;
+        int res = BinarySearch.ficha2ex4(arr, target);
+        System.out.println("Exercio 4: " + (res >= 0 ? "Target found at index: " + res : "Target sould be inserted at index: " + Math.abs(res + 1)));
+    }
+
+
+    public static void Exercicio5() {
+        int[] arr = {3,7,12,15};
+        int target = 100;
+        int res = BinarySearch.ficha2ex4(arr, target);
+        double percentage = ((double)(res<0?Math.abs(res+1):res) / arr.length) * 100;
+        System.out.println("Exercio 5: " + percentage + "%");
+    }
+
+    /**
+     * Calculate the number of elements in the array within the given range.
+     *
+     * @param arr   The sorted array to search.
+     * @param lim_inf   The smallest value in the interval.
+     * @param lim_sup   The highest value in the interval.
+     * @return The number of elements in the given range.
+     */
+    public static int Exercicio6(int []arr , int lim_inf, int lim_sup){
+
+        int posMin = BinarySearch.ficha2ex4(arr, lim_inf);
+        int posMax = BinarySearch.ficha2ex4(arr, lim_sup);
+        int nElements = (posMax<0?Math.abs(posMax+1):posMax)-(posMin<0?Math.abs(posMin+1):posMin);
+        return nElements;
+
 
     }
+
+    public static void main(String[] args) {
+        int[] arr = {3,7,12,15};
+        int target = 0;
+        int lim_inf = 0;
+        int lim_sup = 100;
+        Exercicio1();
+        Exercicio2();
+        Exercicio3();
+        Exercicio4();
+        Exercicio5();
+        {
+            int nElements = Exercicio6(arr,0, 100);
+            System.out.println("Interval ["+lim_inf+","+lim_sup+"]: Contains " +nElements + " elements.");
+        }
+
+
+    }
+
+
 }
